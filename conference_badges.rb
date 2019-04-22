3793fb93d38f867d31f4437dd8e5d5b0d0e0e33b
Stephanie Purple <spurple133@gmail.com>
2019-04-22 22:53:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(array)
+  new=[]
+  array.each_with_index do |item, index|
+    new << "Hello, #{item}! You'll be assigned to room #{index+1}!"
+  end
+  new
+end
